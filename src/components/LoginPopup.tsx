25c756e675f0b6aa5acad4dce1757abcf2764037
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2023-11-21 19:51:41 -0300

    Login Popup of register and login

--- src/components/LoginPopup.tsx
+++ src/components/LoginPopup.tsx
@@ -1,11 +1,35 @@
+import { useState,useEffect } from 'react';
 import styles from '../styles/LoginPopup.module.css'
 
 function LoginPopup() {
+        const [showPopup,setShowPopup]= useState(false)
+        const [timeoutPopup,setTimeoutPopup] = useState(0) 
+        useEffect(() => {
+            if( showPopup === true ){
+                console.log('Exibir Popup!  ' + showPopup);
+            }
+          }, [showPopup]);
 
+        useEffect(()=>{
+            const randomtimeout = Math.floor(Math.random() * 5000000 ) + 100;
+            setTimeoutPopup(randomtimeout);
+        },[]);
+       
+        useEffect(() => {      
+            const popupTimer = setTimeout(() => {
+              setShowPopup(true) 
+            }, timeoutPopup);
+
+            return () => clearTimeout(popupTimer);
+            
+        }, [timeoutPopup,showPopup]);
+       
+         
+        
     return ( 
         <div>
                 <main className={styles.loginPopupMain}>
-                    <div className={styles.loginPopup}>
+                    <div className={styles.loginPopup} style={{display:showPopup ? 'block' : 'none', overflow:showPopup ? 'black':'white'}} >
                         <h1>Faça Login ou Registre-se </h1>
                         <form className={styles.loginForm} action="">
                             <label htmlFor="">
